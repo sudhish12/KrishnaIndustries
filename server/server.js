10ae01e5337886d34144f7236d79e605396ff325
gitCoderNaveen <naveenbsc.mca1518@gmail.com>
2024-07-19 14:51:47 +0530

    Merge upstream changes

--- server/server.js
+++ server/server.js
@@ -39,13 +39,8 @@ const db = mysql.createPool({
   host : 'localhost',
   port : '3306',
   user : 'root',
-<<<<<<< HEAD
-  password : '0210',
+  password : '9043070060',
   database : 'u534462265_crm'
-=======
-  password : 'Rohit@mysql02',
-  database : 'krishnaindustries'
->>>>>>> 3401c8e2fba6bf0d34921d97d0a87de2b0e26463
 })
 
 db.getConnection((err, connection) => {
